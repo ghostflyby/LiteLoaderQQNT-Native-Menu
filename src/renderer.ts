2dc255caa244bc376d8243fb5f8563fdc20ce444
ghostflyby <ghostflyby+git@outlook.com>
2024-02-11 18:29:32 +0800

    use mutationObserver instead of 'contextmenu' event

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,6 +1,9 @@
 // 运行在 Electron 渲染进程 下的页面脚本
-window.addEventListener("contextmenu", (e) => {
-    setTimeout(NativeContextMenu.show, 20)
-}, {
-    passive: false
+
+const observer = new MutationObserver(() => {
+    NativeContextMenu.show()
+})
+
+observer.observe(document.body, {
+    childList: true,
 })
